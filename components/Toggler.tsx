8e5e91927fbc7275dc5dd35951809853f82339c1
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-13 19:36:12 +0800

    feat added darkmode

---
+++ components/Toggler.tsx
@@ -0,0 +1,11 @@
+const Toggler = () => {
+  return (
+    <button className="toggler">
+      <span>light</span>
+      <div />
+      <span>dark</span>
+    </button>
+  );
+};
+
+export default Toggler;
