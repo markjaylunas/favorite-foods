7a56ae3b11429a76fcbfb19d0a4253923865062c
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-13 20:39:28 +0800

    feat add FoodCard image modal

--- components/FoodCard.tsx
+++ components/FoodCard.tsx
@@ -1,15 +1,35 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Food } from "../types/foods";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-
+import { Modal } from "@mantine/core";
 interface Props {
   food: Food;
 }
 
 const FoodCard: FC<Props> = ({ food }) => {
+  const [opened, setOpened] = useState(false);
   return (
     <div className={styles.card}>
+      <div className={styles.modal}>
+        <Modal
+          opened={opened}
+          size="auto"
+          title={food.name}
+          onClose={() => setOpened(false)}
+        >
+          <div className="modal-image">
+            <Image
+              src={`/img/${food.image}`}
+              alt={food.name}
+              width={800}
+              height={600}
+              objectFit="cover"
+              onClick={() => setOpened(true)}
+            />
+          </div>
+        </Modal>
+      </div>
       <div className={styles.image}>
         <Image
           src={`/img/${food.image}`}
@@ -17,6 +37,7 @@ const FoodCard: FC<Props> = ({ food }) => {
           width={600}
           height={400}
           objectFit="cover"
+          onClick={() => setOpened(true)}
         />
       </div>
       <div className={styles.details}>
