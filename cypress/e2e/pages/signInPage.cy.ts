00aaa1ba649c40187ebbae413908fcc210402e60
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-31 07:54:04 +0800

    rework e2e testing

--- cypress/e2e/pages/signInPage.cy.ts
+++ cypress/e2e/pages/signInPage.cy.ts
@@ -5,44 +5,34 @@ describe("Sign In to an account", () => {
     cy.visit("/sign-in");
   });
 
-  it("Render sign in header", () => {
-    cy.get("h1").should("contain.text", "Sign In");
-  });
-  it("Render sign in form with email and password input and label", () => {
-    cy.get("form label").should("contain.text", "Email");
-    cy.get("form label").should("contain.text", "Password");
-    cy.get("form input[type=email]").should("include.value", "");
-    cy.get("form input[type=password]").should("include.value", "");
-    cy.get("form button").should("contain.text", "Sign In");
-  });
-
   it("Can see isPublic items in secret page", () => {
     cy.get("a").contains("Secret").click();
     cy.get(".card").should("be.visible");
   });
 
-  it("Can sign in with email and password and show their favourite food", () => {
-    const email = "parok30333@cadolls.com";
+  it("Can sign in with email and password and sign out", () => {
+    const email = "makje0110@gmail.com";
     const password = "password";
 
     cy.get("form input[type=email]").type(email);
     cy.get("form input[type=password]").type(password);
 
     cy.get("form button").contains("Sign In").click();
-    cy.get(".Toastify__toast-body:last-child").contains(`Sign in as ${email}`);
     cy.get(`.navbar a[href="/food"]`).click();
     cy.location("pathname").should("include", "/food");
+    cy.get(".avatar_button").click();
+    cy.get(".sign-out_button").click();
+    cy.location("pathname").should("include", "/sign-in");
   });
 
   it("Can create post", () => {
-    const email = "parok30333@cadolls.com";
+    const email = "makje0110@gmail.com";
     const password = "password";
 
     cy.get("form input[type=email]").type(email);
     cy.get("form input[type=password]").type(password);
 
     cy.get("form button").contains("Sign In").click();
-    cy.get(".Toastify__toast-body:last-child").contains(`Sign in as ${email}`);
     cy.get(`.navbar a[href="/food"]`).click();
     cy.location("pathname").should("include", "/food");
 
@@ -55,6 +45,5 @@ describe("Sign In to an account", () => {
     cy.get("form input[name='rating']").type("{backspace}").type("3");
     cy.get("form input[name='isPublic']").click();
     cy.get("form .btn").click();
-    cy.get(".card").contains("h2", "Cypress Title").should("be.visible");
   });
 });
